6eb7ba0e416300ccd11e8612bba361b422863dad
Ansh Goyal <anshgoyal1704@gmail.com>
2023-12-13 19:45:08 +0530

    add users pagination
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -10,16 +10,32 @@ const { sendEmail } = require("../config/emailer");
 const { novu } = require("../../server");
 
 exports.getAllUsers = (req, res) => {
-    User.find({}).exec((err, users) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
-        else {
-            res.status(200).send(users);
-            return;
-        }
-    });
+    const { page = 1, limit = 10 } = req.query;
+
+    // Fetch users
+    User.find({})
+        .skip((page - 1) * limit)
+        .limit(Number(limit))
+        .exec((err, users) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+
+            // Count total services
+            User.countDocuments({}, (countErr, totalCount) => {
+                if (countErr) {
+                    res.status(500).send({ message: countErr });
+                    return;
+                }
+
+                // Send both services and total count in the response
+                res.status(200).send({
+                    users,
+                    total: totalCount,
+                });
+            });
+        });
 };
 
 exports.changeUserRole = (req, res) => {
